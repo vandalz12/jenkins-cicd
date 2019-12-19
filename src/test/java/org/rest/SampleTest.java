88553b0a30f3c2f00030844d894d253a850178fa
RESTU MAULUNIDA <vandalz12@gmail.com>
2019-12-19 09:31:24 +0700

    Add Sample Unit Test

---
+++ src/test/java/org/rest/SampleTest.java
@@ -0,0 +1,14 @@
+package org.rest;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class SampleTest {
+
+	@Test
+	public void sampleTest() {
+		Assert.assertEquals(true, true);
+	}
+	
+}
